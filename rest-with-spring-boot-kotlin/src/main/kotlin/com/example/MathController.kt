6f61d30f48f1e38a89d9d2106a6b9fa9fca9b036
Leandro Schillreff <leandroschillreff@gmail.com>
2025-12-10 19:34:48 -0400

    feat: enhance number conversion methods to handle blank inputs and validate numeric format

--- rest-with-spring-boot-kotlin/src/main/kotlin/com/example/MathController.kt
+++ rest-with-spring-boot-kotlin/src/main/kotlin/com/example/MathController.kt
@@ -18,11 +18,15 @@ class MathController {
         return convertDouble(numberOne) + convertDouble(numberTwo)
     }
 
-    private fun convertDouble(number: String): Double {
-        return 0.0
+    private fun convertDouble(strNumber: String): Double {
+        if (strNumber.isNullOrBlank()) return 0.0
+        val number = strNumber.replace(",".toRegex(), ".")
+        return if (isNumeric(number)) number.toDouble() else 0.0
     }
 
-    private fun isNumeric(number: String): Boolean {
-        return false
+    private fun isNumeric(strNumber: String): Boolean {
+        if (strNumber.isNullOrBlank()) return false
+        val number = strNumber.replace(",".toRegex(), ".")
+        return number.matches("""[-+]?[0-9]*\.?[0-9]+""".toRegex())
     }
 }
