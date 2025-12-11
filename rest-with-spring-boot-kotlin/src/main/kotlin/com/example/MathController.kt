0419e67d2b6c7d17a7430a20cf56d6bd525654cb
Leandro Schillreff <leandroschillreff@gmail.com>
2025-12-11 18:54:16 -0400

    feat: add customized exception handling for global error responses

--- rest-with-spring-boot-kotlin/src/main/kotlin/com/example/MathController.kt
+++ rest-with-spring-boot-kotlin/src/main/kotlin/com/example/MathController.kt
@@ -1,5 +1,6 @@
 package com.example
 
+import com.example.exceptions.UnsupportedMathOperationException
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -7,15 +8,48 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class MathController {
 
+    companion object {
+        const val UNSUPPORTED_MATH_OPERATION_EXCEPTION_MESSAGE = "Please set a numeric value!"
+    }
+
     @RequestMapping(value = ["/sum/{numberOne}/{numberTwo}"])
     fun sum(
-        @PathVariable(value = "numberOne") numberOne: String,
-        @PathVariable(value = "numberTwo") numberTwo: String
+        @PathVariable(value = "numberOne") numberOne: String, @PathVariable(value = "numberTwo") numberTwo: String
     ): Double {
-        if (!isNumeric(numberOne) && !isNumeric(numberTwo)) {
-            throw IllegalArgumentException("Please set a numeric value!")
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw UnsupportedMathOperationException(UNSUPPORTED_MATH_OPERATION_EXCEPTION_MESSAGE)
         }
         return convertDouble(numberOne) + convertDouble(numberTwo)
+    }
+
+    @RequestMapping(value = ["/subtraction/{numberOne}/{numberTwo}"])
+    fun subtraction(
+        @PathVariable(value = "numberOne") numberOne: String, @PathVariable(value = "numberTwo") numberTwo: String
+    ): Double {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw UnsupportedMathOperationException(UNSUPPORTED_MATH_OPERATION_EXCEPTION_MESSAGE)
+        }
+        return convertDouble(numberOne) - convertDouble(numberTwo)
+    }
+
+    @RequestMapping(value = ["/multiplication/{numberOne}/{numberTwo}"])
+    fun multiplication(
+        @PathVariable(value = "numberOne") numberOne: String, @PathVariable(value = "numberTwo") numberTwo: String
+    ): Double {
+        if (!isNumeric(numberOne) || isNumeric(numberTwo)) {
+            throw UnsupportedMathOperationException(UNSUPPORTED_MATH_OPERATION_EXCEPTION_MESSAGE)
+        }
+        return convertDouble(numberOne) * convertDouble(numberTwo)
+    }
+
+    @RequestMapping(value = ["/division/{numberOne}/{numberTwo}"])
+    fun division(
+        @PathVariable(value = "numberOne") numberOne: String, @PathVariable(value = "numberTwo") numberTwo: String
+    ): Double {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw UnsupportedMathOperationException(UNSUPPORTED_MATH_OPERATION_EXCEPTION_MESSAGE)
+        }
+        return convertDouble(numberOne) / convertDouble(numberTwo)
     }
 
     private fun convertDouble(strNumber: String): Double {
