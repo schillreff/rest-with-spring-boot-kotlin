2b6aa98afff5248432dac6f38437c74da368a64a
Leandro Schillreff <leandroschillreff@gmail.com>
2025-12-14 17:27:05 -0400

    refactor: rename MathController to PersonController and remove arithmetic operations

---
+++ rest-with-spring-boot-kotlin-person/src/main/kotlin/com/example/controller/PersonController.kt
@@ -0,0 +1,30 @@
+package com.example.controller
+
+import com.example.converters.NumberConverter
+import com.example.exceptions.UnsupportedMathOperationException
+import com.example.math.SimpleMath
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class PersonController {
+
+    companion object {
+        const val UNSUPPORTED_MATH_OPERATION_EXCEPTION_MESSAGE = "Please set a numeric value!"
+    }
+
+    private val math: SimpleMath = SimpleMath()
+
+    @RequestMapping(value = ["/sum/{numberOne}/{numberTwo}"])
+    fun sum(
+        @PathVariable(value = "numberOne") numberOne: String, @PathVariable(value = "numberTwo") numberTwo: String
+    ): Double {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
+            throw UnsupportedMathOperationException(UNSUPPORTED_MATH_OPERATION_EXCEPTION_MESSAGE)
+        }
+        return math.sum(
+            NumberConverter.convertDouble(numberOne), NumberConverter.convertDouble(numberTwo)
+        )
+    }
+}
