ea0cfd6e84efbe253b84180a842ce22ccbed0cb8
Leandro Schillreff <leandroschillreff@gmail.com>
2025-12-14 17:25:31 -0400

    feat: add initial application setup with exception handling and basic math operations

---
+++ rest-with-spring-boot-kotlin-math/src/main/kotlin/com/example/math/SimpleMath.kt
@@ -0,0 +1,13 @@
+package com.example.math
+
+class SimpleMath {
+
+    fun sum(numberOne: Double, numberTwo: Double) = numberOne + numberTwo
+
+    fun subtraction(numberOne: Double, numberTwo: Double) = numberOne - numberTwo
+
+    fun multiplication(numberOne: Double, numberTwo: Double) = numberOne + numberTwo
+
+    fun division(numberOne: Double, numberTwo: Double) = numberOne + numberTwo
+
+}
