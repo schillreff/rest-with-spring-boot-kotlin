cf9c16df24de862e6d7f55aedbb8b0740c6a24c8
Leandro Schillreff <leandroschillreff@gmail.com>
2025-12-17 17:47:25 -0400

    feat: add gender property to Person class

--- rest-with-spring-boot-kotlin-person/src/main/kotlin/com/example/model/Person.kt
+++ rest-with-spring-boot-kotlin-person/src/main/kotlin/com/example/model/Person.kt
@@ -4,5 +4,6 @@ data class Person (
     var id: Long = 0,
     var firstName: String = "",
     var lastName: String = "",
-    var address: String = ""
+    var address: String = "",
+    var gender: String = ""
 )
