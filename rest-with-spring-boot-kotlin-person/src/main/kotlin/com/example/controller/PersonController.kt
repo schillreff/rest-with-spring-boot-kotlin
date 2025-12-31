1c34b81800cc2b252b800a42d242bc4ab02f91a8
Leandro Schillreff <leandroschillreff@gmail.com>
2025-12-31 18:35:31 -0400

    feat: change findById method to create method in PersonController for person creation

--- rest-with-spring-boot-kotlin-person/src/main/kotlin/com/example/controller/PersonController.kt
+++ rest-with-spring-boot-kotlin-person/src/main/kotlin/com/example/controller/PersonController.kt
@@ -5,6 +5,7 @@ import com.example.services.PersonService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
@@ -29,10 +30,10 @@ class PersonController {
     }
 
     @RequestMapping(method = [RequestMethod.POST], produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun findById(
-        @PathVariable(value = "id") id: Long
+    fun create(
+        @RequestBody person: Person
     ): Person {
-        return service.findById(id)
+        return service.create(person)
     }
 
 }
