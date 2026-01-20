b5de50b8ab1fb3837f7b10de3b7f2a6ddddfed97
Leandro Schillreff <leandroschillreff@gmail.com>
2026-01-20 19:51:33 -0400

    fix: add JPA Id annotation to id field in Person data class

--- rest-with-spring-boot-kotlin-person/src/main/kotlin/com/example/model/Person.kt
+++ rest-with-spring-boot-kotlin-person/src/main/kotlin/com/example/model/Person.kt
@@ -1,11 +1,14 @@
 package com.example.model
 
 import jakarta.persistence.Entity
+import jakarta.persistence.Id
 import jakarta.persistence.Table
 
 @Entity
 @Table(name = "person")
 data class Person(
+    @Id
+
     var id: Long = 0,
     var firstName: String = "",
     var lastName: String = "",
