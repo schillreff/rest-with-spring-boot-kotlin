bd9ebe7dceb6345ebc96bdc2965a57514f3651a4
Leandro Schillreff <leandroschillreff@gmail.com>
2026-01-20 19:51:52 -0400

    fix: add JPA GeneratedValue annotation to id field in Person data class

--- rest-with-spring-boot-kotlin-person/src/main/kotlin/com/example/model/Person.kt
+++ rest-with-spring-boot-kotlin-person/src/main/kotlin/com/example/model/Person.kt
@@ -1,6 +1,8 @@
 package com.example.model
 
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Table
 
@@ -8,7 +10,7 @@ import jakarta.persistence.Table
 @Table(name = "person")
 data class Person(
     @Id
-
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long = 0,
     var firstName: String = "",
     var lastName: String = "",
