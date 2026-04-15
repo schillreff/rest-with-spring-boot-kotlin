09698e230abc52ee30a0767b555952eb4d17fb67
Leandro Schillreff <leandroschillreff@gmail.com>
2026-04-15 19:25:13 -0400

    feat: implement PersonRepository interface extending JpaRepository

--- rest-with-spring-boot-kotlin-person/src/main/kotlin/com/example/repository/PersonRepository.kt
+++ rest-with-spring-boot-kotlin-person/src/main/kotlin/com/example/repository/PersonRepository.kt
@@ -1,4 +1,8 @@
 package com.example.repository
 
-interface PersonRepository {
-}
+import com.example.model.Person
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface PersonRepository : JpaRepository<Person, Long?>
